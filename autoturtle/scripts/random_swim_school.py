173c62cd0168aef6ad940849ef386fa3defb13a7
tobi1modna <tobia.poppi@virgilio.it>
2020-11-18 19:07:56 +0100

    ex 1b completed - random_swimschool

--- autoturtle/scripts/random_swim_school.py
+++ autoturtle/scripts/random_swim_school.py
@@ -2,64 +2,72 @@
 import rospy
 import math
 import time
+import random
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
-import random
+from turtlesim.srv import TeleportAbsolute
 
 class SwimSchool():
-
+    #try to teleport the turtle with rosservice and givng the destination position gave by user.
     posizione = Pose()
+    faseiniziale = True
+    init_pose = Pose()
 
     def update_pose(self, data):
         self.posizione.x = round(data.x, 2)
         self.posizione.y = round(data.y, 2)
+        if(self.faseiniziale):
+            self.init_pose.x = self.posizione.x
+            self.init_pose.y = self.posizione.y
+            rospy.loginfo("Teleported turtle in position:")
+            print 'x: ',self.init_pose.x,'     y: ',self.init_pose.y
+            self.faseiniziale  = not self.faseiniziale
+
 
     def __init__(self):
-        init_posx = random.uniform(0, 10)
-        init_posy = random.uniform(0, 10)
         rospy.init_node('SwimSchool', anonymous=False)
-        rospy.loginfo(" Press CTRL+c to stop moving the Turtle")
+        rospy.Subscriber('/turtle1/pose', Pose, self.update_pose)
+
+        xran = random.uniform(2, 8)
+        yran = random.uniform(2, 8)
+
+        teleService = rospy.ServiceProxy('/turtle1/teleport_absolute', TeleportAbsolute)
+        teleService(xran, yran, 0)
+
         rospy.on_shutdown(self.shutdown)
         self.cmd_vel = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
-        rospy.Subscriber('/turtle1/pose', Pose, self.update_pose)
-        init_pose = Pose()
-        init_pose.x = round(init_posx, 2)
-        init_pose.y = round(init_posx, 2)
 
-        init_move = Twist()
-        init_move.linear.x = init_posx
-        init_move.linear.y = init_posy
-
-        rate = rospy.Rate(300)
+        rate = rospy.Rate(300);
 
         rospy.loginfo("Set rate 1Hz")
         move_cmd = Twist()
+
         move_cmd.linear.x = input("Linear velocity: ")
         move_cmd.angular.z = input("Angular velocity: ")
         timetowait = round((2*math.pi)/abs(move_cmd.angular.z), 2)
+        print('time to wait = ',timetowait)
         change = False
         start = time.time()
-        #check = True
-        self.cmd_vel.publish(init_move)
+
         while not rospy.is_shutdown():
-            #print(self.posizione.x, self.posizione.y)
+
             if change:
                 move_cmd.angular.z = -(move_cmd.angular.z)
                 change = not change
                 start = time.time()
+
             self.cmd_vel.publish(move_cmd)
-            print (self.posizione.x)
-            print (self.posizione.y)
-            #if check:
-                #init_pose.x = self.posizione.x
-                #init_pose.y = self.posizione.y
-                #print(init_pose.x)
-                #print(init_pose.y)
-                #check = not check
             end = time.time()
-            d = end - start
-            if (round(d, 2) > timetowait - 1) and (self.posizione.x >= (init_pose.x - 0.05) and self.posizione.x <= (init_pose.x)) and (self.posizione.y > (init_pose.y - 0.1) and self.posizione.y < (init_pose.y + 0.1)):
+            delta = end - start
+
+            if ((round(delta, 2) > timetowait-0.5)
+              and self.posizione.x >= (self.init_pose.x - 0.05)
+              and self.posizione.x <= (self.init_pose.x)
+              and self.posizione.y > (self.init_pose.y - 0.1)
+              and self.posizione.y < (self.init_pose.y + 0.1)):
                 change = not change
+                print 'swapping direction'
+
             rate.sleep()
 
     def shutdown(self):
