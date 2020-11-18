6ed0bbc35ae8d22212a77a4f307c5047e5fdc3f8
tobi1modna <tobia.poppi@virgilio.it>
2020-11-18 16:24:05 +0100

    modified structure

--- build/autoturtle/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tobi/Documenti/catkin_ws/src/autoturtle
-# Build directory: /home/tobi/Documenti/catkin_ws/build/autoturtle
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
