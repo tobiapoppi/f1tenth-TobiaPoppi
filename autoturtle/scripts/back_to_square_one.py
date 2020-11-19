04c3a690a6483c413c70d67ccec559f9acca3e20
tobi1modna <tobia.poppi@virgilio.it>
2020-11-19 17:20:06 +0100

    still not working

--- autoturtle/scripts/back_to_square_one.py
+++ autoturtle/scripts/back_to_square_one.py
@@ -1,44 +1,115 @@
 #!/usr/bin/env python
 import rospy
-from geometry_msgs import Twist
+import time
+from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 from turtlesim.srv import Spawn
+from turtlesim.srv import Kill
+from turtlesim.msg import Color
+from std_srvs.srv import Empty
 
 class Square():
     posizione = Pose()
-    faseiniziale = True
-    init_pose = Pose()
 
-    # def update_pose(self, data):
-    #     self.posizione.x = round(data.x, 2)
-    #     self.posizione.y = round(data.y, 2)
-    #     if(self.faseiniziale):
-    #         self.init_pose.x = self.posizione.x
-    #         self.init_pose.y = self.posizione.y
-    #         rospy.loginfo("Teleported turtle in position:")
-    #         print 'x: ',self.init_pose.x,'     y: ',self.init_pose.y
-    #         self.faseiniziale  = not self.faseiniziale
+    def changeBackground(self):
+        rospy.wait_for_service('/clear')
+        try:
+            rospy.set_param('/turtlesim/background_r', 255)
+            rospy.set_param('/turtlesim/background_g', 50)
+            rospy.set_param('/turtlesim/background_b', 50)
+            changeCol = rospy.ServiceProxy('/clear', Empty)
+            changeCol()
 
+        except rospy.ServiceException as e:
+            rospy.loginfo('Service failed with the exception {e}')
+
+
+    def update_pose(self, data):
+        self.posizione.x = round(data.x, 3)
+        self.posizione.y = round(data.y, 3)
+
+    def rotate(self, count):
+        if count == 1:
+            self.move_cmd.linear.x = 0
+            self.move_cmd.linear.y = 2
+        if count == 2:
+            self.move_cmd.linear.x = -2
+            self.move_cmd.linear.y = 0
+        if count == 3:
+            self.move_cmd.linear.x = 0
+            self.move_cmd.linear.y = -2
+        if count == 0:
+            self.move_cmd.linear.x = 2
+            self.move_cmd.linear.y = 0
 
     def __init__(self):
-        # rospy.Subscriber('/turtle1/pose', Pose, self.update_pose)
-        # rospy.init_node('SwimSchool', anonymous=False)
 
-        length = input("Input float to determine quare's length")
+        rospy.init_node('turtle_Square', anonymous=False)
 
-        # teleService = rospy.ServiceProxy('/turtle1/teleport_absolute', TeleportAbsolute)
-        # teleService(xran, yran, 0)
+        length = input("Input float to determine quare's length: ")
+        self.changeBackground()
+
+        killService = rospy.ServiceProxy('/kill', Kill)
+        spawnService = rospy.ServiceProxy('/spawn', Spawn)
+        killService('turtle1')
+        spawnService(1, 1, 0, 'turtle1')
 
         rospy.on_shutdown(self.shutdown)
-        self.cmd_vel = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+        self.cmd_vel = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=5)
 
         rate = rospy.Rate(300);
 
         rospy.loginfo("Set rate 300Hz")
-        move_cmd = Twist()
+        self.move_cmd = Twist()
+        self.move_cmd.linear.x = 2
+        self.move_cmd.angular.z = 0
+
+        rospy.Subscriber('/turtle1/pose', Pose, self.update_pose)
+        isX = True
+        count = 0
+
+        rospy.loginfo("Starting in 3..2..1..")
+        rospy.sleep(3)
 
         while not rospy.is_shutdown():
-
+            if isX:
+                start = time.time()
+                travelled = 0 #distance travelled right now
+                print self.posizione.x
+                while (self.posizione.x < (lenght-0.005)):
+                    print self.posizione.x , 'ciao'
+                    self.cmd_vel.publish(self.move_cmd)
+                    end = time.time()
+                    travelled = travelled + abs(self.move_cmd.linear.x * (end - start))
+                    start = time.time()
+                    left = length - travelled #strada rimasta da percorrere
+                    if left <= (length*20/100):
+                        if abs(self.move_cmd.linear.x) > 0.3:
+                            x = (left*100)/(length*20/100)
+                            self.move_cmd.linear.x = (self.move_cmd.linear.x * x)/100
+                    rate.sleep()
+            else:
+                start = time.time()
+                travelled = 0 #distance travelled right now
+                while travelled < length:
+                    self.cmd_vel.publish(self.move_cmd)
+                    end = time.time()
+                    travelled = travelled + abs(self.move_cmd.linear.y * (end - start))
+                    start = time.time()
+                    left = length - travelled #strada rimasta da percorrere
+                    if left <= (length*20/100):
+                        if abs(self.move_cmd.linear.y) > 0.3:
+                            x = (left*100)/(length*20/100)
+                            self.move_cmd.linear.y = (self.move_cmd.linear.y * x)/100
+                    rate.sleep()
+            print self.posizione.x
+            isX = not isX
+            if count == 3:
+                count = 0
+            else:
+                count += 1
+            self.rotate(count)
+            print travelled
 
     def shutdown(self):
         rospy.loginfo("Stopping the turtle")
